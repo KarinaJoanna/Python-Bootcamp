2d5cf4de148a6d75719964e7eeb4770dec77011a
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-05 15:02:30 -0600

    Odd or Even code

---
+++ D3/1OddOrEven.py
@@ -0,0 +1,8 @@
+print("Odd or Even")
+#Get the number
+number = int(input("Enter a number: "))
+#Check if the number is odd or even
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
