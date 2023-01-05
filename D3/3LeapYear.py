e922cd82ac7d3d39766d17e05053f1976c3585da
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-05 15:24:43 -0600

    Leap Year code

---
+++ D3/3LeapYear.py
@@ -0,0 +1,16 @@
+# Leap Year
+# This program determines if a year is a leap year
+
+print("Leap Year")
+#Get the year
+year = int(input("Enter a year: "))
+#Check if the year is a leap year
+if year % 4 == 0:
+    print("Leap year")
+elif year % 100 == 0:
+        print("Not leap year")
+elif year % 400 == 0:
+            print("Leap year")
+else:
+    print("Not leap year")
+
