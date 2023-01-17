28f8212f50cd883dbb088fde0fa5b336a883fcb7
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-17 10:17:44 -0600

    Nested lists code examples code

---
+++ D4/5NestedLists.py
@@ -0,0 +1,10 @@
+#Nested Lists
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+#Nested Lists
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
