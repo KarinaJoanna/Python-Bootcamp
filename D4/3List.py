1d119d771d6ef2771d975bd3f1558ecd1471ef1d
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-17 09:53:26 -0600

    Lists examples code

---
+++ D4/3List.py
@@ -0,0 +1,19 @@
+#Lists
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+#print(states_of_america[0])
+
+#Modify the list
+#states_of_america[1] = "Pencilvania"
+#print(states_of_america)
+
+#Add to the list
+#states_of_america.append("Puerto Rico")
+#print(states_of_america)
+
+#Add to the list - extending list with another list
+states_of_america.extend(["Canada", "Mexico"])
+print(states_of_america)
+
+#Check Python documentation for more list methods
