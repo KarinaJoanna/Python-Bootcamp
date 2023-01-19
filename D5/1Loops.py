ba1dd1e3a1ce9f62a759967d0d89346a532b7e74
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-19 12:55:22 -0600

    Loops examples code

---
+++ D5/1Loops.py
@@ -0,0 +1,8 @@
+#For loop
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+    print(fruits) #prints the list of fruits each time the loop runs
+print(fruits) #prints the list of fruits once the loop is done
