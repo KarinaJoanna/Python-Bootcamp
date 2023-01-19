72be61600c691b2995a48a2554874124359315dd
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-19 13:07:31 -0600

    Highest Score V2 using max() and min() functions

---
+++ D5/3HighestScore2.py
@@ -0,0 +1,9 @@
+#Highest Score without loops, using max() and min() functions
+
+student_scores = input("Input a list of student scores \n").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+print(max(student_scores))
+print(min(student_scores))
