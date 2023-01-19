4d39719974d1cc9134a031df49d45493236c9a6f
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-19 13:21:11 -0600

    For loops and the range() function

---
+++ D5/4ForLoopsAndRangeFunction.py
@@ -0,0 +1,12 @@
+# For loops and the range() function
+
+# For loop with range
+#for number in range (1, 11, 3): #range is a function that creates a sequence of numbers from 1 to 10
+                                # the third parameter is the step size, which is 3
+ #   print(number)
+
+# Gauss adding 1 to 100
+total = 0
+for numer in range(1, 101):
+    total += numer
+print(total)
