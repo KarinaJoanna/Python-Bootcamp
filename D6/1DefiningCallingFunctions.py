5a2c9972938fc17317744543032daf2632a0683a
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-20 15:17:55 -0600

    Defining and Calling Functions

---
+++ D6/1DefiningCallingFunctions.py
@@ -0,0 +1,11 @@
+# Defining and Calling Functions in Python
+
+# Make our own function
+
+# Defining a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Calling a function
+my_function()
