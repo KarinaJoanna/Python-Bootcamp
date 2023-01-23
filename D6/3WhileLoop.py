f4e065cdbadd2301ede00a51b8dd7535cc71bcd2
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-23 12:58:02 -0600

    While loop code

---
+++ D6/3WhileLoop.py
@@ -0,0 +1,29 @@
+# Using while loop for the Hurdles challenge in Reeborg's World
+
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+
+#def jump():    
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+#num_hurdles = 6
+# while num_hurdles > 0:
+#   jump()
+#   num_hurdles -= 1
+#   print(num_hurdles)
+
+
+# Using this while for search the goal 
+
+#while not at_goal():
+#    jump()
+
