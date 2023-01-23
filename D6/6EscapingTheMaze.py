26fe27d2ff01f2237099c828ef2e944c57b26f72
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-23 14:13:58 -0600

    Escaping the Maze code V1

---
+++ D6/6EscapingTheMaze.py
@@ -0,0 +1,16 @@
+# Escaping the Maze - Reeborg's World
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
