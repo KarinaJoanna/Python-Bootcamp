b42a79a42de709f100a7a19bef862cc8e661624f
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-01-26 11:43:42 -0600

    Paint Area Calculator

---
+++ D8/2PaintAreaCalculator.py
@@ -0,0 +1,18 @@
+# Paint Area Calculator
+
+# 1 can of paint can cover 5 square meters of wall
+
+# Result should be rounded up to the nearest whole number
+
+import math
+
+def paint_calc(height, width, cover):
+    num_cans = (height * width) / cover
+    round_up_cans = math.ceil(num_cans)
+    print(f"You'll need {round_up_cans} cans of paint.")
+    
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
